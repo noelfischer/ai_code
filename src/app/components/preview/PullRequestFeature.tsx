26f8be05c88f394d964ada93d096a9f2e5d91f5a
Noel Fischer <noelfischer@gmx.ch>
2023-08-27 15:48:28 +0200

    Add glow and Reveal

--- src/app/components/preview/PullRequestFeature.tsx
+++ src/app/components/preview/PullRequestFeature.tsx
@@ -4,6 +4,8 @@ import { createTheme } from '@mui/material/styles';
 import styles from './page.module.css'
 import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
 import DifferenceOutlinedIcon from '@mui/icons-material/DifferenceOutlined';
+import Reveal from 'react-awesome-reveal';
+import { keyframes } from "@emotion/react";
 
 
 const theme = createTheme();
@@ -38,33 +40,50 @@ const PullRequestFeature = () => {
     setSelectedTab(newValue);
   };
 
+  const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(10px) scale(0.97, 0.97);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0)  scale(1, 1);
+  }
+`;
+
   return (
-    <Container>
-      <Paper elevation={0} className={styles.container} square>
-        <Tabs
-          value={selectedTab}
-          onChange={handleTabChange}
-          TabIndicatorProps={{
-            style: {
-              top: 9,
-              bottom: "unset",
-            }
-          }
-          }
-        >
-          <Tab icon={<QuestionAnswerOutlinedIcon className={styles.svg} />} iconPosition="start" label={
-            <>Conversation <span className={styles.badge}>5</span></>
-          } className={styles.tab}></Tab>
-          <Tab icon={<DifferenceOutlinedIcon className={styles.svg} />} iconPosition="start" label={
-            <>Files changed <span className={styles.badge}>2</span></>
-          } className={styles.tab} />
-        </Tabs>
-      </Paper>
-      <Paper elevation={0} className={styles.content} square>
-        {selectedTab === 0 && <ConversationTabContent />}
-        {selectedTab === 1 && <FilesChangedTabContent />}
-      </Paper>
-    </Container>
+    <>
+      <div className={styles.glow}></div>
+      <Reveal fraction={0.5} duration={400} keyframes={customAnimation} triggerOnce={false}>
+        <Container>
+          <Paper elevation={0} className={styles.container} square>
+            <Tabs
+              value={selectedTab}
+              onChange={handleTabChange}
+              TabIndicatorProps={{
+                style: {
+                  top: 9,
+                  bottom: "unset",
+                }
+              }
+              }
+            >
+              <Tab icon={<QuestionAnswerOutlinedIcon className={styles.svg} />} iconPosition="start" label={
+                <>Conversation <span className={styles.badge}>5</span></>
+              } className={styles.tab}></Tab>
+              <Tab icon={<DifferenceOutlinedIcon className={styles.svg} />} iconPosition="start" label={
+                <>Files changed <span className={styles.badge}>2</span></>
+              } className={styles.tab} />
+            </Tabs>
+          </Paper>
+          <Paper elevation={0} className={styles.content} square>
+            {selectedTab === 0 && <ConversationTabContent />}
+            {selectedTab === 1 && <FilesChangedTabContent />}
+          </Paper>
+        </Container>
+      </Reveal>
+    </>
   );
 };
 
