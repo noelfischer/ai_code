2f7cf3d655749180fc0a2bea8d85d65854e6ba7e
Noel Fischer <noelfischer@gmx.ch>
2023-08-31 16:55:49 +0200

    Add file header

--- src/app/components/preview/FilesChanged.tsx
+++ src/app/components/preview/FilesChanged.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { Typography, Box, Input, InputAdornment, TextField, Button, ListItemButton, ListItemText, ListItemIcon, Collapse, List } from '@mui/material';
+import { Typography, Box, TextField, ListItemButton, ListItemText, ListItemIcon, Collapse, List, FormControlLabel, Checkbox, Stack, IconButton } from '@mui/material';
 import styles from './page.module.css'
 import SearchIcon from '@mui/icons-material/Search';
-import { ExpandMore, StarBorder } from '@mui/icons-material';
+import { ExpandMore } from '@mui/icons-material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
 import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
+import ExpandOutlinedIcon from '@mui/icons-material/ExpandOutlined';
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 export const FilesChangedTabContent = () => {
     const [open, setOpen] = useState(true);
@@ -13,8 +18,8 @@ export const FilesChangedTabContent = () => {
         setOpen(!open);
     };
     return (
-        <Box className={styles.filesChangedContainer}>
-            <Box sx={{ maxWidth: 270 }}>
+        <Stack direction="row" spacing={2} sx={{ paddingBlock: 1 }}>
+            <Box sx={{ maxWidth: 270, display: { xs: 'none', md: 'block' } }}>
                 <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                     <SearchIcon fontSize='small' sx={{ color: 'action.active', ml: 2, mr: 1, my: 0.5 }} />
                     <TextField fullWidth size="small" variant="standard" label="Filter changed files" className={styles.fileSearch} />
@@ -36,7 +41,28 @@ export const FilesChangedTabContent = () => {
                     </Collapse>
                 </Box>
             </Box>
-            <Box className={styles.file}>
+            <Box sx={{ width: "100%" }}>
+                <Stack direction="row" alignItems="center" className={styles.fileHeader}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <IconButton><ExpandMore fontSize='small' className={styles.darkGrey} /></IconButton>
+                        <IconButton><ExpandOutlinedIcon fontSize='small' className={styles.darkGrey} /></IconButton>
+                        <Typography className={styles.darkGrey} sx={{ fontSize: 12 }}><pre>40</pre></Typography>
+                        <Stack direction="row" spacing={"1px"} alignItems="center">
+                            <Box className={styles.green} />
+                            <Box className={styles.green} />
+                            <Box className={styles.green} />
+                            <Box className={styles.green} />
+                            <Box className={styles.neutral} />
+                        </Stack>
+                        <Typography sx={{ fontSize: 15 }}><pre>src/image_compression.py</pre></Typography>
+                        <IconButton><ContentCopyRoundedIcon fontSize='small' className={styles.darkGrey} /></IconButton>
+                    </Stack>
+                    <Stack direction="row" spacing={0} alignItems="center" sx={{ width: "100%" }} justifyContent="flex-end">
+                        <FormControlLabel className={styles.checkbox} control={<Checkbox size="small" />} label="Viewed" />
+                        <IconButton><ChatBubbleOutlineOutlinedIcon fontSize='small' className={styles.darkGrey} /></IconButton>
+                        <IconButton><MoreVertIcon fontSize='small' className={styles.darkGrey} /></IconButton>
+                    </Stack>
+                </Stack>
             </Box>
-        </Box>)
+        </Stack>)
 }
