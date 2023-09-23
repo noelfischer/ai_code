8bed89f350e4a5b85704c3c2ff482886c7105abd
Noel Fischer <noelfischer@gmx.ch>
2023-09-23 15:24:27 +0200

    Quick layout fix

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -17,15 +17,17 @@ export default function Hero() {
             <h1 className={styles.title + " palette.text.primary"}>Get reviews <span className={styles.span}>instantly</span><br />with AI Expert</h1>
             <p className={styles.p}>Open a pull request and get valuable insights on bugs and problems<br /> with suggested code solutions in seconds.</p>
             <div className={styles.btncontainer} ref={ref}>
-                <Button href='/signup' className={styles.button1} variant="contained" color='primary' endIcon={
-                    <div className={styles.container}>
-                        <ArrowForwardIosIcon className={styles.icon} />
-                        <ArrowForwardIosIcon className={styles.icon} />
-                        <ArrowForwardIosIcon className={styles.icon} />
-                    </div>}
-                    aria-label="Get started">
-                    Get started
-                </Button>
+                <a href='/signup'>
+                    <Button className={styles.button1} variant="contained" color='primary' endIcon={
+                        <div className={styles.container}>
+                            <ArrowForwardIosIcon className={styles.icon} />
+                            <ArrowForwardIosIcon className={styles.icon} />
+                            <ArrowForwardIosIcon className={styles.icon} />
+                        </div>}
+                        aria-label="Get started">
+                        Get started
+                    </Button>
+                </a>
                 <Button onClick={handleClick} className={styles.button2} variant='outlined' color='primary' endIcon={
                     <div className={styles.container}>
                         <ArrowForwardIosIcon className={styles.icon} />
